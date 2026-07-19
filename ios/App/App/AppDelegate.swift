cbb289c951ff96e466fcd877252dac76b92cbcca
jake <jakedryka1@gmail.com>
2026-07-19 10:40:13 +0800

    Remove user activity continuation method
    Removed the user activity continuation method from AppDelegate.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -31,9 +31,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity)
-    }
-
 }
