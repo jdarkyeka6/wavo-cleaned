97101c8dcfff5f01312d22e1aad65f62588b7741
Jake D <jakedryka1@gmail.com>
2026-09-05 23:19:57 +0800

    Add Wavo Live Activity model

---
+++ ios/App/WavoLiveActivity/WavoActivityAttributes.swift
@@ -0,0 +1,16 @@
+import Foundation
+import ActivityKit
+
+struct WavoActivityAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        var subtitle: String
+        var detail: String
+        var participantCount: Int
+        var startsAt: Date
+        var endsAt: Date
+    }
+
+    var kind: String
+    var title: String
+    var deepLink: String
+}
